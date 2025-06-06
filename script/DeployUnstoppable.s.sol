7940bfee24d638df1f8550cbc6769a1d247a31f9
Dhruv Jain <dhruvjain2905@gmail.com>
2025-06-06 16:17:55 -0400

    added files

---
+++ script/DeployUnstoppable.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import "forge-std/Script.sol";
+import {UnstoppableVault} from "../src/UnstoppableVault.sol";
+import {UnstoppableMonitor} from "../src/UnstoppableMonitor.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {DamnValuableToken} from "../DamnValuableToken.sol";
+
+contract DeployUnstoppableVaultAndMonitor is Script {
+    function run() external {
+        vm.startBroadcast(); 
+
+
+        DamnValuableToken token = new DamnValuableToken();
+        console.log("DamnValuableToken deployed at:", address(token));
+
+
+        address owner = msg.sender;
+        address feeRecipient = vm.addr(1337); // can also make this another address
+
+        UnstoppableVault vault = new UnstoppableVault(ERC20(address(token)), owner, feeRecipient);
+        console.log("UnstoppableVault deployed at:", address(vault));
+
+
+        UnstoppableMonitor monitor = new UnstoppableMonitor(address(vault));
+        console.log("UnstoppableMonitor deployed at:", address(monitor));
+
+        vm.stopBroadcast();
+    }
+}
