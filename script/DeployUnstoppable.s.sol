ec64188981779709ce7feeaf1f83d524e970418a
Dhruv Jain <dhruvjain2905@gmail.com>
2025-09-15 02:29:47 -0400

    Funded player

--- script/DeployUnstoppable.s.sol
+++ script/DeployUnstoppable.s.sol
@@ -8,6 +8,7 @@ import {ERC20} from "solmate/tokens/ERC20.sol";
 import {DamnValuableToken} from "../DamnValuableToken.sol";
 
 uint256 constant TOKENS_IN_VAULT = 1_000_000e18;
+uint256 constant INITIAL_PLAYER_TOKEN_BALANCE = 10e18;
 
 contract DeployUnstoppableVaultAndMonitor is Script {
     function run() external {
@@ -24,6 +25,9 @@ contract DeployUnstoppableVaultAndMonitor is Script {
 
         token.approve(address(vault), TOKENS_IN_VAULT);
         vault.deposit(TOKENS_IN_VAULT, address(owner));
+
+        // Fund player's account with initial token balance
+        token.transfer(0x4C1f023A2A914d109bEa600aB518f3078466e279, INITIAL_PLAYER_TOKEN_BALANCE);
 
         UnstoppableMonitor monitor = new UnstoppableMonitor(address(vault));
         console.log("UnstoppableMonitor deployed at:", address(monitor));
